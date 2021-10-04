cdc49dd0e56f9031f749b08ca374a2864b84d57b
TDOETaan <tdoegit@gmail.com>
2021-10-04 12:52:43 -0400

    Finished Week 2 assignment.

--- index.js
+++ index.js
@@ -10,25 +10,31 @@ document.addEventListener("DOMContentLoaded", function(){
     ];
 
     var imageArrayIndex = 0;
+        
+        var leftButton = document.querySelector("#leftButton");
+        var rightButton = document.querySelector("#rightButton");
+        var imgJS = document.getElementById("imgJS");
 
-    var leftButton = document.querySelector("#leftButton");
-    var rightButton = document.querySelector("#rightButton");
-    var imgJS = document.getElementById("imgJS");
+        //src = imageArray[0]
+        imgJS.src = imageArray[imageArrayIndex];
 
-    //src = imageArray[0]
-    imgJS.src = imageArray[imageArrayIndex];
-
-    rightButton.addEventListener('click', function(){
-        imageArrayIndex++; //increment 0 to 1
-        imgJS.src = imageArray[imageArrayIndex]; //src =imageArray{1}
+        rightButton.addEventListener('click', function(){
+            if(imageArrayIndex < imageArray.length - 1){
+                imageArrayIndex++; //increment 0 to 1
+                imgJS.src = imageArray[imageArrayIndex]; // src = imageArray[1]
+            }
+            else{
+                console.log("Out of Bounds on right button!")
+            }
+        })
+        
+        leftButton.addEventListener('click', function(){
+            if(imageArrayIndex > 0){
+                imageArrayIndex--; //increment 1 to 0
+                imgJS.src = imageArray[imageArrayIndex]; // src = imageArray[0]
+            }
+            else{
+                console.log("Out of Bounds on left button!")
+            }
+        })
     })
-
-    leftButton.addEventListener('click', function(){
-        if(imageArrayIndex > 0){
-
-        imageArrayIndex--; //increment 1 to 0
-        imgJS.src = imageArray[imageArrayIndex]; //src =imageArray{0}
-        }
-    })
-
-})
